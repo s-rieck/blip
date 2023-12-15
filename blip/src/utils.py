a25c413981b091cf6dda741a19fa281da7d679f7
s-rieck <rieck094@umn.edu>
2023-12-15 11:42:19 -0600

    utils.py bug fix
    Adding to dictionary for generating suffixes when using multiple models to include galaxy and fixedgalaxy models.

--- blip/src/utils.py
+++ blip/src/utils.py
@@ -55,6 +55,8 @@ def gen_suffixes(names):
     shorthand = {'noise':{'abbrv':'','count':1},
                  'isgwb':{'abbrv':'I','count':1},
                  'sph':{'abbrv':'A','count':1},
+                 'galaxy':{'abbrv':'G','count':1},
+                 'fixedgalaxy':{'abbrv':'F','count':1},
                  'population':{'abbrv':'P','count':1},
                  'hierarchical':{'abbrv':'H','count':1} }
     
